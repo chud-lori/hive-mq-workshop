6e39597bb9b8569d075c127540a320b166dc48fa
chud-lori <imlori000@gmail.com>
2025-02-03 14:57:22 +0700

    add argument

--- web/publ.py
+++ web/publ.py
@@ -1,8 +1,11 @@
+import sys
 import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
    print(f"Connected with result code {rc}")
 
+
+value = sys.argv[1]
 mqtt_client = mqtt.Client()
 mqtt_client.on_connect = on_connect
 mqtt_client.username_pw_set('bangke', 'Bangke123')
@@ -10,7 +13,7 @@ mqtt_client.tls_set()
 mqtt_client.connect("f8416f89ec1142c9b72d7b1070b0a0e9.s1.eu.hivemq.cloud", 8883)
 
 mqtt_client.loop_start()
-mqtt_client.publish("esp32/bangke", "on", 1)
+mqtt_client.publish("esp32/bangke", value, 1)
 mqtt_client.loop_stop()
 mqtt_client.disconnect()
 
